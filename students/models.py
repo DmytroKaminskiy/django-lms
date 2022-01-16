81f7ac518fee43114ee688d4ef692b964ebdc69f
Valeriy Zmievskoy <valeriy@adbeat.com>
2022-01-16 12:53:28 +0200

    Created view function for filtering studends

--- students/models.py
+++ students/models.py
@@ -1,4 +1,7 @@
+import random
+
 from django.db import models
+from faker import Faker
 
 
 # Create your models here.
@@ -6,3 +9,19 @@ class Student(models.Model):
     first_name = models.CharField(max_length=64, null=False)
     last_name = models.CharField(max_length=64, null=False)
     age = models.IntegerField(default=20)
+
+    @classmethod
+    def generate_students(cls, count):
+        faker = Faker()
+
+        for _ in range(count):
+            s = Student()
+            s.first_name = faker.first_name()
+            s.last_name = faker.last_name()
+            s.age = random.randint(18, 80)
+
+            s.save()
+
+    def __str__(self):
+        return f"Student({self.id}) {self.first_name} {self.last_name} " \
+               f"{self.age}"
--- students/views.py
+++ students/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse
+
+from students.models import Student
+from students.utils import render_list
+
+
+def get_students(request):
+    qs = Student.objects.all()
+    params = [
+        'first_name',
+        'first_name__startswith',
+        'first_name__endswith',
+        'first_name__contains',
+        'last_name',
+        'age',
+        'age__gt',
+        'age__lt',
+    ]
+
+    query = {}
+
+    for param_name in params:
+        param_value = request.GET.get(param_name)
+        if param_value:
+            query[param_name] = param_value
+
+    try:
+        qs = qs.filter(**query)
+    except ValueError as e:
+        return HttpResponse(f"Error wrong input data. {str(e)}", status=400)
+
+    return render_list(qs)
